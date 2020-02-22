3d449387bcb1902ea414fa86c151358189f9e53e
Guillermo Bandrés Catalina <mail@gbandres.com>
2020-02-22 13:27:39 +0100

    Heroku deployment error

--- io/index.js
+++ io/index.js
@@ -15,7 +15,9 @@ export default function() {
 
     // overwrite nuxt.server.listen()
     this.nuxt.server.listen = (port, host) =>
-      new Promise(resolve => server.listen(port || 3000, "0.0.0.0"));
+      new Promise(resolve =>
+        server.listen(port || 3000, host || "localhost", resolve)
+      );
     // close this server on 'close' event
     this.nuxt.hook("close", () => new Promise(server.close));
 
